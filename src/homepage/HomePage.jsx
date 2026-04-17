3c0bde1b67015f32069117c4a30ac6d24b61cb3a
Rakibul611852 <rakibulislam611852@gmail.com>
2026-04-17 21:23:38 +0600

    error handleing

---
+++ src/homepage/HomePage.jsx
@@ -0,0 +1,13 @@
+
+import AllFriends from '../components/shared/homepage/AllFriends';
+import Banner from '../components/shared/homepage/Banner';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Banner/>
+      <AllFriends/>
+    </div>
+  )
+}
+export default HomePage;
